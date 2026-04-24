f599c39ae449f30f156541d91925f0bb8478b75c
dk <kudin.dima@gmail.com>
2026-04-24 03:21:03 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Butter | Fort Lauderdale Café + Community',
+  description: 'Butter is a design-forward coffee and retail concept built for culture, taste, and everyday ritual in Fort Lauderdale.',
+  openGraph: {
+    "title": "Butter Fort Lauderdale",
+    "description": "Modern cafe, retail platform, and community space.",
+    "siteName": "Butter"
+  },
+};
+
+const dmSans = DM_Sans({ variable: "--font-dm-sans", subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} antialiased`}>
+          
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
